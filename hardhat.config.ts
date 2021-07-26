cab13b33f7caf7eb99ebcf22c362e031b1cc2913
Hanul <hanul@hanul.me>
2021-07-26 14:38:50 +0900

    change to rinkeby

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,10 +9,10 @@ const config: HardhatUserConfig = {
     version: "0.8.5",
   },
   networks: {
-    kovan: {
-      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [process.env.ADMIN || ''],
-      chainId: 42,
+      chainId: 4,
     },
   },
   etherscan: {
