1e714473262e187599bb38720222d35c54757268
Hanul <hanul@hanul.me>
2021-07-26 15:00:53 +0900

    Create UniswapV3.sol

---
+++ contracts/test/UniswapV3.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.5;
+
+import {ISwapRouter as _ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+
+interface ISwapRouter is _ISwapRouter {}
